eb7399f5dc6406767d49bc06e11fe92d0e10dffd
neodevelop <juan@makingdevs.com>
2020-05-22 20:23:59 -0500

    Impl deploy

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,7 @@
 #!/bin/zsh
 
+mkdir -p ~/neodevelop
+cp -R * ~/neodevelop
+cd ~/neodevelop
 ~/.rbenv/shims/bundle exec ruby shutdown.rb
 ~/.rbenv/shims/bundle exec ruby app_neodevelop.rb -p 1234 -o 0.0.0.0 &
